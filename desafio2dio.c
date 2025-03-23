5754c6bd237e064d48a96863faabbe43b5c5c653
Pedro Estevam <pedro.estevam@sempreceub.com>
2025-03-23 12:11:35 -0300

    Update desafio2dio.c

--- desafio2dio.c
+++ desafio2dio.c
@@ -1,4 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
+const char* VitoriasJogador(int SaldoJogador) {
+    if (SaldoJogador < 11) return "ferro";
+    else if (SaldoJogador <= 20) return "bronze";
+    else if (SaldoJogador <= 30) return "prata";
+    else if (SaldoJogador <= 40) return "ouro";
+    else if (SaldoJogador <= 50) return "platina";
+    else if (SaldoJogador <= 60) return "diamante";
+    else if (SaldoJogador <= 70) return "lendario";
+    else return "imortal";
+}
+int main (){
+    int vitorias, derrotas, SaldoJogador;
+    printf("Digite o numero de vitorias:");
+    scanf("%d", &vitorias);
+    printf("Digite o numero de derrotas:");
+    scanf("%d", &derrotas);
+    SaldoJogador = vitorias - derrotas;
+    printf("O heroi tem saldo de %d e esta no nivel de %s\n", SaldoJogador, VitoriasJogador(SaldoJogador));
+    
+    return 0;
+    
+    
+}
 
